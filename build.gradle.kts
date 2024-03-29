68d23587a224e0fa98b7fa8ef9d15d100067d67f
ThiagoROX <51332006+SrBedrock@users.noreply.github.com>
2024-03-29 13:51:41 -0300

    Adicionado build workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -195,7 +195,7 @@ tasks {
 
     compileJava.get().dependsOn(clean)
     build.get().dependsOn(shadowJar)
-    build.get().dependsOn(publishToMavenLocal)
+//    build.get().dependsOn(publishToMavenLocal)
 }
 
 bukkit {
