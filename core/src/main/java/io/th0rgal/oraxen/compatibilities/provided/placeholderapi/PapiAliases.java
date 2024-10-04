31dc869e9ca4e93a4c0f7d9d9443d983195cfeba
ThiagoROX <51332006+SrBedrock@users.noreply.github.com>
2024-10-04 15:06:03 -0300

    Adicionado placeholder no itemName

--- core/src/main/java/io/th0rgal/oraxen/compatibilities/provided/placeholderapi/PapiAliases.java
+++ core/src/main/java/io/th0rgal/oraxen/compatibilities/provided/placeholderapi/PapiAliases.java
@@ -15,21 +15,25 @@ public class PapiAliases {
     private PapiAliases() {}
 
     @NotNull
-    public static String setPlaceholders(Player player, String text) {
+    public static String setPlaceholders(final Player player, final String text) {
         return PlaceholderAPI.setPlaceholders(player, text);
     }
 
     @NotNull
     @Contract("_, _ -> param2")
-    public static ItemStack setPlaceholders(Player player, @NotNull ItemStack item) {
-        ItemMeta meta = item.getItemMeta();
-        if (meta != null) {
+    public static ItemStack setPlaceholders(final Player player, @NotNull final ItemStack item) {
+
+        if (item.hasItemMeta()) {
+            final ItemMeta meta = item.getItemMeta();
             if (meta.hasDisplayName())
                 meta.setDisplayName(setPlaceholders(player, meta.getDisplayName()));
-            List<String> itemLore = meta.getLore();
+            if (meta.hasItemName()) {
+                meta.setItemName(setPlaceholders(player, meta.getItemName()));
+            }
+            final List<String> itemLore = meta.getLore();
             if (itemLore != null && !itemLore.isEmpty()) {
-                List<String> lore = new ArrayList<>();
-                for (var line : itemLore) {
+                final List<String> lore = new ArrayList<>();
+                for (final var line : itemLore) {
                     lore.add(setPlaceholders(player, line));
                 }
                 meta.setLore(lore);
